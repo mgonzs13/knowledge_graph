d2a048786bf5a2cd0c6a00d0d5f2bc8374372db5
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-12-02 09:44:04 +0100

    fixing viewer format and removing test from CMakeLists of terminal

--- knowledge_graph_viewer/knowledge_graph_viewer/main.py
+++ knowledge_graph_viewer/knowledge_graph_viewer/main.py
@@ -6,8 +6,12 @@ from rqt_gui.main import Main
 
 
 def main():
-    sys.exit(Main().main(
-        sys.argv, standalone="knowledge_graph_viewer.knowledge_graph_plugin.KnowledgeGraphPlugin"))
+    sys.exit(
+        Main().main(
+            sys.argv,
+            standalone="knowledge_graph_viewer.knowledge_graph_plugin.KnowledgeGraphPlugin",
+        )
+    )
 
 
 if __name__ == "__main__":
--- knowledge_graph_viewer/setup.py
+++ knowledge_graph_viewer/setup.py
@@ -1,38 +1,36 @@
 from setuptools import setup, find_packages
 
-package_name = 'knowledge_graph_viewer'
+package_name = "knowledge_graph_viewer"
 setup(
     name=package_name,
-    version='0.2.0',
+    version="0.2.0",
     # package_dir={'': ''},
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name + '/resource',
-         ['resource/KnowledgeGraphPlugin.ui']),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['plugin.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name + "/resource", ["resource/KnowledgeGraphPlugin.ui"]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name, ["plugin.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    author='Francisco Martin',
-    maintainer='Miguel Ángel González Santamarta',
-    maintainer_email='mgons@unileon.es',
+    author="Francisco Martin",
+    maintainer="Miguel Ángel González Santamarta",
+    maintainer_email="mgons@unileon.es",
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
     ],
     description=(
-        'knowledge_graph_viewer provides a GUI plugin for visualizing the BICA graph.'
+        "knowledge_graph_viewer provides a GUI plugin for visualizing the BICA graph."
     ),
-    license='BSD',
-    tests_require=['pytest'],
+    license="BSD",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'rqt_knowledge_graph = knowledge_graph_viewer.main:main',
+        "console_scripts": [
+            "rqt_knowledge_graph = knowledge_graph_viewer.main:main",
         ],
     },
 )
